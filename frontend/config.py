3ed6a540e799d482ce44506c8610b019a1fc6ea0
fr0st-even <iosef.joelcv@gmail.com>
2025-05-29 12:54:59 -0600

    Delete frontend/config.py

--- frontend/config.py
+++
@@ -1,6 +0,0 @@
-import os
-
-class Config:
-    DATA_FOLDER = './data'
-    DATABASE_FILE = os.path.join(DATA_FOLDER, 'database.json') # ./data/database.json
-    SQLITE_DB = os.path.join(DATA_FOLDER, 'users.db') # ./data/users_db
